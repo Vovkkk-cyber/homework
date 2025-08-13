9ee5e4641ee4cb0bc1b3c17a5747d02dd8be8472
Vovkkk-cyber <vov.blindowscky@yandex.ru>
2025-08-13 12:47:26 +0300

    Модульный JS и npm

---
+++ buttonEl.js
@@ -0,0 +1,53 @@
+import { renderComments } from './renderComments.js'
+import { comments } from './coments.js'
+
+export const buttonEl = document.getElementById('button')
+buttonEl.addEventListener('click', () => {
+    const nameEl = document.getElementById('name')
+    const commentEl = document.getElementById('comment')
+    const name = nameEl.value
+        .trim()
+        .replaceAll('<', '&lt;')
+        .replaceAll('>', '&gt;')
+    const comment = commentEl.value
+        .trim()
+        .replaceAll('<', '&lt;')
+        .replaceAll('>', '&gt;')
+
+    nameEl.classList.remove('error')
+    commentEl.classList.remove('error')
+
+    if (nameEl.value === '') {
+        nameEl.classList.add('error')
+    }
+    if (commentEl.value === '') {
+        commentEl.classList.add('error')
+    }
+    if (nameEl.value === '' || commentEl.value === '') {
+        return
+    }
+
+    const now = new Date()
+    const options = {
+        year: 'numeric',
+        month: 'numeric',
+        day: 'numeric',
+        hour: 'numeric',
+        minute: 'numeric',
+        hour12: false,
+    }
+    const dateString = now.toLocaleString('ru-RU', options).replace(',', '')
+
+    const newComment = {
+        id: comments.length + 1,
+        name: name,
+        text: comment,
+        date: dateString,
+        likeCount: 0,
+        liked: false,
+    }
+
+    comments.push(newComment)
+    console.log(comments)
+    renderComments()
+})
---
+++ renderComments.js
@@ -0,0 +1,35 @@
+import { comments } from './coments.js'
+import { handleLikes, addInitReplyListeners } from './handler.js'
+
+export function renderComments() {
+    const commentsList = document.getElementById('comments')
+    commentsList.innerHTML = ''
+    const commentHtml = comments
+        .map((comment) => {
+            const classString = `like-button ${comment.liked ? '-active-like' : ''}`
+            const newComment = `
+      <li data-id="${comment.id}" class="comment" >
+        <div class="comment-header">
+          <div class="comment-name">${comment.name}</div>
+          <div>${comment.date}</div>
+        </div>
+        <div class="comment-body">
+          <div class="comment-text">
+            ${comment.text}
+          </div>
+        </div>
+        <div class="comment-footer">
+          <div class="likes">
+            <span class="likes-counter">${comment.likeCount}</span>
+            <button data-id="${comment.id}" class="${classString}"></button>
+          </div>
+        </div>
+      </li>
+        `
+            return newComment
+        })
+        .join('')
+    commentsList.innerHTML = commentHtml
+    handleLikes()
+    addInitReplyListeners()
+}
