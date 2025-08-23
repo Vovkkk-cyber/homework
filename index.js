72b105cecb0117d7c761f4dec10fead93ed19225
Vovkkk-cyber <vov.blindowscky@yandex.ru>
2025-08-23 16:14:59 +0300

    h4

--- index.js
+++ index.js
@@ -2,3 +2,10 @@ import { renderComments } from './renderComments.js'
 import { buttonEl } from './buttonEl.js'
 buttonEl
 renderComments()
+fetch('https://wedev-api.sky.pro/api/v1/:personal-key/comments')
+    .then((response) => {
+        return response.json()
+    })
+    .then((data) => {
+        console.log(data)
+    })
